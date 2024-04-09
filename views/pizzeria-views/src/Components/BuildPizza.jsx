ee3c6b6032d1be8fa58266599df7d163f2e4f16c
Aayush895 <aayush895@gmail.com>
2024-04-09 20:24:44 +0530

    Done with build pizza component

--- views/pizzeria-views/src/Components/BuildPizza.jsx
+++ views/pizzeria-views/src/Components/BuildPizza.jsx
@@ -3,6 +3,8 @@ import {useState, useEffect} from 'react'
 const BuildPizza = () => {
 
   const [ingredients, setIngredients] = useState(null)
+  const [isChecked, setisChecked] = useState([])
+  const [total, setTotal] = useState(0)
 
   useEffect(() => {
     const fetchIngredients = async () => {
@@ -14,37 +16,80 @@ const BuildPizza = () => {
     }
 
     fetchIngredients()
+    if (ingredients !== null) {
+      setisChecked(new Array(ingredients.length).fill(false))
+    }
   }, [])
 
+  const handleChecked = (position) => {
+    const updatedCheckedState = isChecked.map((checked, index) => {
+      return position === index ? !checked : checked
+    })
+
+    setisChecked(updatedCheckedState)
+    const totalPrice = updatedCheckedState.reduce(
+      (sum, currentState, index) => {
+        if (currentState === true) {
+          return sum + ingredients[index].price
+        }
+        return sum
+      },
+      0
+    )
+
+    setTotal(totalPrice)
+  }
+
   return (
-    <div className='p-16 text-center'>
-      <p className='mb-10'>Pizzeria now gives you option to build your own pizza. Customize your pizza by choosing ingredients from the list below.</p>
+    <div className="p-16 text-center">
+      <p className="mb-10">
+        Pizzeria now gives you option to build your own pizza. Customize your
+        pizza by choosing ingredients from the list below.
+      </p>
       <table>
-        {ingredients !== null ? ingredients.map((ingredient) => {
-         return (
-           <tr key={ingredient?._id}>
-             <td>
-               <img
-                 src={ingredient?.image}
-                 alt=""
-                 width="150px"
-                 height="150px"
-               />
-             </td>
-             <td className='text-center font-bold'>
-               <span>{ingredient?.name}</span>
-               <span className='mx-5'>&#8360;. {ingredient?.price}.00</span>
-             </td>
-             <td>
-               <input type="checkbox" name="add" />
-               <label htmlFor="add" className='mx-4 text-amber-500'>Add</label>
-             </td>
-           </tr>
-         )
-        }) : <h1>Loading...</h1>}
+        <tbody>
+          {ingredients !== null ? (
+            ingredients.map((ingredient, index) => {
+              return (
+                <tr key={ingredient?._id}>
+                  <td>
+                    <img
+                      src={ingredient?.image}
+                      alt=""
+                      width="150px"
+                      height="150px"
+                    />
+                  </td>
+                  <td className="text-center font-bold">
+                    <span>{ingredient?.name}</span>
+                    <span className="mx-5">
+                      &#8360;. {ingredient?.price}.00
+                    </span>
+                  </td>
+                  <td>
+                    <input
+                      type="checkbox"
+                      name="add"
+                      checked={isChecked[index]}
+                      onChange={() => handleChecked(index)}
+                    />
+                    <label htmlFor="add" className="mx-4 text-amber-500">
+                      Add
+                    </label>
+                  </td>
+                </tr>
+              )
+            })
+          ) : (
+            <tr>
+              <td>Loading...</td>
+            </tr>
+          )}
+        </tbody>
       </table>
 
-      <button className='bg-black text-white border border-amber-500 rounded px-4 py-3 mt-10'>
+      <h1 className="text-4xl font-bold mt-10">Total: &#8360;{total}.00</h1>
+      <button className="bg-black text-white border border-amber-500 rounded px-4 py-3 mt-10">
         Build your Pizza
       </button>
     </div>
